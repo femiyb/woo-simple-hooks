156189aef6667a3403b61138b89237d6c0f92166
Femi <94sam.fem@gmail.com>
2020-05-16 22:05:21 +0200

    Tested up to latest WP and WC versions and Language files added

--- woocommerce-simple-hooks.php
+++ woocommerce-simple-hooks.php
@@ -7,11 +7,11 @@
  * Plugin Name:       	Simple Hooks for WooCommerce
  * Plugin URI:        	http://femiyb.co.za
  * Description:       	This plugin makes it easier for you to add WooCommerce hooks, so if you don't know your way around php, you can easily add the hooks from the dashboard.
- * Version:           	1.0.2
+ * Version:           	1.1
  * Requires at least: 	4.4
- * Tested up to: 		5.1
+ * Tested up to: 		5.4.1
  * WC requires at least:3.0.0
- * WC tested up to: 	3.5.5
+ * WC tested up to: 	4.1.0
  * Author:            	Femi
  * Author URI:        	http://www.femiyb.com
  * License:            	GPL-2.0+
@@ -25,6 +25,11 @@
  * dashboard-specific hooks, and public-facing site hooks.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-simple-hooks.php';
+
+function wcsh_load_plugin_text_domain() {
+	load_plugin_textdomain( ' woo-simple-hooks', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
+}
+add_action( 'plugins_loaded', 'wcsh_load_plugin_text_domain' );
 
 
 function run_wcsh_Loader() {
@@ -89,90 +94,96 @@ function update_wcsh_settings() {
 if( !function_exists("wcsh_settings_page") ) {
 	
 function wcsh_settings_page(){
-	$shophooks = array("Before Shop Page Main Content"=>"wcsh_before_main_content",
-					   "Shop Page Archive Description"=>"wcsh_archive_description",
-					   "Shop Page Before Shop Loop Hook"=>"wcsh_before_shop_loop",
-					   "Shop Page Before Shop Loop Item"=>"wcsh_before_shop_loop_item",
-					   "Shop Page Before Shop Loop Item Title"=>"wcsh_before_shop_loop_item_title",
-					   "Shop Page Shop Loop Item Title"=>"wcsh_shop_loop_item_title",
-					   "Shop Page After Shop Loop Item Title"=>"wcsh_after_shop_loop_item_title",
-					   "Shop Page After Shop Loop Item"=>"wcsh_after_shop_loop_item",
-					   "Shop Page After Shop Loop"=>"wcsh_after_shop_loop"
-					  );
+	$shophooks = array(
+					''. __('Before Shop Page Main Content', 'woo-simple-hooks') .''=>"wcsh_before_main_content",
+					''. __('Shop Page Archive Description', 'woo-simple-hooks') .''=>"wcsh_archive_description",
+					''. __('Shop Page Before Shop Loop Hook', 'woo-simple-hooks') .''=>"wcsh_before_shop_loop",
+					''. __('Shop Page Before Shop Loop Item', 'woo-simple-hooks') .''=>"wcsh_before_shop_loop_item",
+					''. __('Shop Page Before Shop Loop Item Title', 'woo-simple-hooks') .''=>"wcsh_before_shop_loop_item_title",
+					''. __('Shop Page Shop Loop Item Title', 'woo-simple-hooks') .''=>"wcsh_shop_loop_item_title",
+					''. __('Shop Page After Shop Loop Item Title', 'woo-simple-hooks') .''=>"wcsh_after_shop_loop_item_title",
+					''. __('Shop Page After Shop Loop Item', 'woo-simple-hooks') .''=>"wcsh_after_shop_loop_item",
+					''. __('Shop Page After Shop Loop', 'woo-simple-hooks') .''=>"wcsh_after_shop_loop"
+					);
 	
-	$singleproducthooks = array("Single Product Before single product hook"=>"wcsh_before_single_product",
-								"Single Product Before single product Summary hook"=>"wcsh_before_single_product_summary",
-							   	"Single Product Summary hook"=>"wcsh_single_product_summary",
-							   	"Single Product Simple Add to Cart hook"=>"wcsh_wcsh_add_to_cart",
-								"Single Product Before Add to Cart Form hook"=>"wcsh_before_add_to_cart_form",
-								"Single Product Before Add to Cart Button hook"=>"wcsh_before_add_to_cart_button",
-								"Single Product Before Add to Cart Quantity hook"=>"wcsh_before_add_to_cart_quantity",
-								"Single Product After Add to Cart Quantity hook"=>"wcsh_after_add_to_cart_quantity",
-								"Single Product After Add to Cart Button hook"=>"wcsh_after_add_to_cart_button",
-								"Single Product After Add to Cart Form hook"=>"wcsh_after_add_to_cart_form",
-								"Single Product Thumbnails hook"=>"wcsh_product_thumbnails",
-								"Single Product After Product Summary hook"=>"wcsh_after_single_product_summary",
-								"Single Product Meta Start hook"=>"wcsh_product_meta_start",
-								"Single Product Meta End hook"=>"wcsh_product_meta_end"
+	$singleproducthooks = array(
+					''. __('Single Product Before single product hook', 'woo-simple-hooks') .''=>"wcsh_before_single_product",
+					''. __('Single Product Before single product Summary hook', 'woo-simple-hooks') .''=>"wcsh_before_single_product_summary",
+					''. __('Single Product Summary hook', 'woo-simple-hooks') .''=>"wcsh_single_product_summary",
+					''. __('Single Product Simple Add to Cart hook', 'woo-simple-hooks') .''=>"wcsh_wcsh_add_to_cart",
+					''. __('Single Product Before Add to Cart Form hook', 'woo-simple-hooks') .''=>"wcsh_before_add_to_cart_form",
+					''. __('Single Product Before Add to Cart Button hook', 'woo-simple-hooks') .''=>"wcsh_before_add_to_cart_button",
+					''. __('Single Product Before Add to Cart Quantity hook', 'woo-simple-hooks') .''=>"wcsh_before_add_to_cart_quantity",
+					''. __('Single Product After Add to Cart Quantity hook', 'woo-simple-hooks') .''=>"wcsh_after_add_to_cart_quantity",
+					''. __('Single Product After Add to Cart Button hook', 'woo-simple-hooks') .''=>"wcsh_after_add_to_cart_button",
+					''. __('Single Product After Add to Cart Form hook', 'woo-simple-hooks') .''=>"wcsh_after_add_to_cart_form",
+					''. __('Single Product Thumbnails hook', 'woo-simple-hooks') .''=>"wcsh_product_thumbnails",
+					''. __('Single Product After Product Summary hook', 'woo-simple-hooks') .''=>"wcsh_after_single_product_summary",
+					''. __('Single Product Meta Start hook', 'woo-simple-hooks') .''=>"wcsh_product_meta_start",
+					''. __('Single Product Meta End hook', 'woo-simple-hooks') .''=>"wcsh_product_meta_end"
+					);
 
-							   );
-
 	
-	$cartpagehooks = array("Cart Page Before Cart Hook"=>"wcsh_before_cart",
-						   "Cart Page Before Cart Table Hook"=>"wcsh_before_cart_table",
-						   "Cart Page Before Cart Contents"=>"wcsh_before_cart_contents",
-						   "Cart Page Cart Contents"=>"wcsh_cart_contents",
-						   "Cart Page Cart Coupon"=>"wcsh_cart_coupon",
-						   "Cart Page Cart Actions Hook"=>"wcsh_cart_actions",
-						   "Cart Page After Cart Contents Hook"=>"wcsh_after_cart_contents",
-						   "Cart Page After Cart Table Hook"=>"wcsh_after_cart_table",
-						   "Cart Page Before Cart Totals Hook"=>"wcsh_before_cart_totals",
-						   "Cart Page Cart Totals Before Shipping Hook"=>"wcsh_cart_totals_before_shipping",
-						   "Cart Page After Shipping Rate"=>"wcsh_after_shipping_rate",
-						   "Cart Page Before Shipping Calculator"=>"wcsh_before_shipping_calculator",
-						   "Cart Page After Shipping Calculator"=>"wcsh_after_shipping_calculator",
-						   "Cart Page Cart Totals After Shipping"=>"wcsh_cart_totals_after_shipping",
-						   "Cart Page Cart Totals Before Order Total"=>"wcsh_cart_totals_before_order_total",
-						   "Cart Page Cart Totals After Order Total"=>"wcsh_cart_totals_after_order_total",
-						   "Cart Page Proceed To Checkout"=>"wcsh_proceed_to_checkout",
-						   "Cart Page After Cart Totals"=>"wcsh_after_cart_totals",
-						   "Cart Page After Cart"=>"wcsh_after_cart",
-						   "Checkout Page Review Order Before Payment Hook"=>"wcsh_review_order_before_payment"
-						   );
+	$cartpagehooks = array(
+					''. __('Cart Page Before Cart Hook', 'woo-simple-hooks') .''=>"wcsh_before_cart",
+					''. __('Cart Page Before Cart Table Hook', 'woo-simple-hooks') .''=>"wcsh_before_cart_table",
+					''. __('Cart Page Before Cart Contents', 'woo-simple-hooks') .''=>"wcsh_before_cart_contents",
+					''. __('Cart Page Cart Contents', 'woo-simple-hooks') .''=>"wcsh_cart_contents",
+					''. __('Cart Page Cart Coupon', 'woo-simple-hooks') .''=>"wcsh_cart_coupon",
+					''. __('Cart Page Cart Actions Hook', 'woo-simple-hooks') .''=>"wcsh_cart_actions",
+					''. __('Cart Page After Cart Contents Hook', 'woo-simple-hooks') .''=>"wcsh_after_cart_contents",
+					''. __('Cart Page After Cart Table Hook', 'woo-simple-hooks') .''=>"wcsh_after_cart_table",
+					''. __('Cart Page Before Cart Totals Hook', 'woo-simple-hooks') .''=>"wcsh_before_cart_totals",
+					''. __('Cart Page Cart Totals Before Shipping Hook', 'woo-simple-hooks') .''=>"wcsh_cart_totals_before_shipping",
+					''. __('Cart Page After Shipping Rate', 'woo-simple-hooks') .''=>"wcsh_after_shipping_rate",
+					''. __('Cart Page Before Shipping Calculator', 'woo-simple-hooks') .''=>"wcsh_before_shipping_calculator",
+					''. __('Cart Page After Shipping Calculator', 'woo-simple-hooks') .''=>"wcsh_after_shipping_calculator",
+					''. __('Cart Page Cart Totals After Shipping', 'woo-simple-hooks') .''=>"wcsh_cart_totals_after_shipping",
+					''. __('Cart Page Cart Totals Before Order Total', 'woo-simple-hooks') .''=>"wcsh_cart_totals_before_order_total",
+					''. __('Cart Page Cart Totals After Order Total', 'woo-simple-hooks') .''=>"wcsh_cart_totals_after_order_total",
+					''. __('Cart Page Proceed To Checkout', 'woo-simple-hooks') .''=>"wcsh_proceed_to_checkout",
+					''. __('Cart Page After Cart Totals', 'woo-simple-hooks') .''=>"wcsh_after_cart_totals",
+					''. __('Cart Page After Cart', 'woo-simple-hooks') .''=>"wcsh_after_cart",
+					''. __('Checkout Page Review Order Before Payment Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_before_payment"
+					);
 	
-	$accountpagehooks = array("Account Page Login Form Start Hook"=>"wcsh_login_form_start",
-							  "Account Page Before Customer Login Hook"=>"wcsh_before_customer_login_form",
-							 "Checkout Page Login Form Hook"=>"wcsh_login_form",
-							 "Checkout Page Login Form End Hook"=>"wcsh_login_form_end",
-							 "Checkout Page After Customer Login Form Hook"=>"wcsh_after_customer_login_form");
+	$accountpagehooks = array(
+					''. __('Account Page Login Form Start Hook', 'woo-simple-hooks') .''=>"wcsh_login_form_start",
+					''. __('Account Page Before Customer Login Hook', 'woo-simple-hooks') .''=>"wcsh_before_customer_login_form",
+					''. __('Checkout Page Login Form Hook', 'woo-simple-hooks') .''=>"wcsh_login_form",
+					''. __('Checkout Page Login Form End Hook', 'woo-simple-hooks') .''=>"wcsh_login_form_end",
+					''. __('Checkout Page After Customer Login Form Hook', 'woo-simple-hooks') .''=>"wcsh_after_customer_login_form"
+					);
 	
-	$checkoutpagehooks = array("Checkout Page Before Checkout Form"=>"wcsh_before_checkout_form",
-							  "Checkout Page Before Customer Details"=>"wcsh_checkout_before_customer_details",
-							  "Checkout Page After Customer Details"=>"wcsh_checkout_after_customer_details",
-							  "Checkout Page Checkout Billing Hook"=>"wcsh_checkout_billing",
-							  "Checkout Page Before Checkout Billing Hook"=>"wcsh_before_checkout_billing_form",
-							  "Checkout Page Before Order Review Hook"=>"wcsh_checkout_before_order_review",
-							  "Checkout Page Review Order Before Cart Contents Hook"=>"wcsh_review_order_before_cart_contents",
-							  "Checkout Page Review Order After Cart Contents Hook"=>"wcsh_review_order_after_cart_contents",
-							  "Checkout Page Review Before Shipping Hook"=>"wcsh_review_order_before_shipping",
-							  "Checkout Page After Shipping Rate Hook"=>"wcsh_after_shipping_rate",
-							  "Checkout Page Review Order After Shipping Hook"=>"wcsh_review_order_after_shipping",
-							  "Checkout Page Review Order Before Order Total Hook"=>"wcsh_review_order_before_order_total",
-							  "Checkout Page Review Order After Order Total Hook"=>"wcsh_review_order_after_order_total",
-							  "Checkout Page After Checkout Billing Form Hook"=>"wcsh_after_checkout_billing_form",
-							  "Checkout Page Before Checkout registration Form Hook"=>"wcsh_before_checkout_registration_form",
-							  "Checkout Page Before Terms and Conditions Hook"=>"wcsh_checkout_before_terms_and_conditions",
-							  "Checkout Page Review Order After Submit Hook"=>"wcsh_review_order_after_submit",
-							  "Checkout Page Review Order After Payment Hook"=>"wcsh_review_order_after_payment",
-							  "Checkout Page After Checkout Form Hook"=>"wcsh_after_checkout_form"
-							  );
+	$checkoutpagehooks = array(
+					''. __('Checkout Page Before Checkout Form', 'woo-simple-hooks') .''=>"wcsh_before_checkout_form",
+					''. __('Checkout Page Before Customer Details', 'woo-simple-hooks') .''=>"wcsh_checkout_before_customer_details",
+					''. __('Checkout Page After Customer Details', 'woo-simple-hooks') .''=>"wcsh_checkout_after_customer_details",
+					''. __('Checkout Page Checkout Billing Hook', 'woo-simple-hooks') .''=>"wcsh_checkout_billing",
+					''. __('Checkout Page Before Checkout Billing Hook', 'woo-simple-hooks') .''=>"wcsh_before_checkout_billing_form",
+					''. __('Checkout Page Before Order Review Hook', 'woo-simple-hooks') .''=>"wcsh_checkout_before_order_review",
+					''. __('Checkout Page Review Order Before Cart Contents Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_before_cart_contents",
+					''. __('Checkout Page Review Order After Cart Contents Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_after_cart_contents",
+					''. __('Checkout Page Review Before Shipping Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_before_shipping",
+					''. __('Checkout Page After Shipping Rate Hook', 'woo-simple-hooks') .''=>"wcsh_after_shipping_rate",
+					''. __('Checkout Page Review Order After Shipping Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_after_shipping",
+					''. __('Checkout Page Review Order Before Order Total Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_before_order_total",
+					''. __('Checkout Page Review Order After Order Total Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_after_order_total",
+					''. __('Checkout Page After Checkout Billing Form Hook', 'woo-simple-hooks') .''=>"wcsh_after_checkout_billing_form",
+					''. __('Checkout Page Before Checkout registration Form Hook', 'woo-simple-hooks') .''=>"wcsh_before_checkout_registration_form",
+					''. __('Checkout Page Before Terms and Conditions Hook', 'woo-simple-hooks') .''=>"wcsh_checkout_before_terms_and_conditions",
+					''. __('Checkout Page Review Order After Submit Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_after_submit",
+					''. __('Checkout Page Review Order After Payment Hook', 'woo-simple-hooks') .''=>"wcsh_review_order_after_payment",
+					''. __('Checkout Page After Checkout Form Hook', 'woo-simple-hooks') .''=>"wcsh_after_checkout_form"
+					);
 	
-	$otherhooks = array("Before Mini Cart Hook"=>"wcsh_before_mini_cart",
-						"Before Mini Cart Contents Hook"=>"wcsh_before_mini_cart_contents"
+	$otherhooks = array(
+					''. __('Before Mini Cart Hook', 'woo-simple-hooks') .''=>"wcsh_before_mini_cart",
+					''. __('Before Mini Cart Contents Hook', 'woo-simple-hooks') .''=>"wcsh_before_mini_cart_contents"
 					   );
 ?>
 		<?php $settings_image = plugin_dir_url( __FILE__ ) . 'icon-256x256.png'; ?>
-  		<h1 style="font-family: monospace;text-transform: uppercase;">Simple Hooks Settings</h1>
+  		<h1 style="font-family: monospace;text-transform: uppercase;"><?php _e('Simple Hooks Settings', 'woo-simple-hooks'); ?></h1>
   		<img src="<?php echo $settings_image; ?>" style="width: 4%;">
   		<form method="post" action="options.php">
   			<style>
@@ -204,7 +215,7 @@ function wcsh_settings_page(){
 				settings_fields( 'wcsh-settings-singleproduct' );
 				do_settings_sections( 'wcsh-settings-singleproduct' );
     		 ?>
-	  			<h2>WOOCOMMERCE SINGLE PRODUCT PAGE HOOKS</h2>
+	  			<h2><?php _e('WOOCOMMERCE SINGLE PRODUCT PAGE HOOKS', 'woo-simple-hooks'); ?></h2>
 	  		<?php
 			
 				foreach ($singleproducthooks as $key => $singleproducthook){ ?>
@@ -220,7 +231,7 @@ function wcsh_settings_page(){
 				settings_fields( 'wcsh-settings-cartpage' );
 				do_settings_sections( 'wcsh-settings-cartpage' );
 				?>
-				<h2>WOOCOMMERCE CART PAGE HOOKS</h2>
+				<h2><?php _e('WOOCOMMERCE CART PAGE HOOKS', 'woo-simple-hooks'); ?></h2>
 				<?php
 				foreach ($cartpagehooks as $key => $cartpagehook){ ?>
 				<table class="form-table">
@@ -236,7 +247,7 @@ function wcsh_settings_page(){
 				settings_fields( 'wcsh-settings-checkoutpage' );
 				do_settings_sections( 'wcsh-settings-checkoutpage' );
 				?>
-				<h2>WOOCOMMERCE CHECKOUT PAGE HOOKS</h2>
+				<h2><?php _e('WOOCOMMERCE CHECKOUT PAGE HOOKS', 'woo-simple-hooks'); ?></h2>
 				<?php
 				foreach ($checkoutpagehooks as $key => $checkoutpagehook){ ?>
 				<table class="form-table">
@@ -252,7 +263,7 @@ function wcsh_settings_page(){
     		settings_fields( 'wcsh-settings-accountpage' );
     		do_settings_sections( 'wcsh-settings-accountpage' );
     		?>
-	  		<h2>WOOCOMMERCE ACCOUNT PAGE HOOKS</h2>
+	  		<h2><?php _e('WOOCOMMERCE ACCOUNT PAGE HOOKS', 'woo-simple-hooks'); ?></h2>
 	  		<?php
 	  		foreach ($accountpagehooks as $key => $accountpagehook){ ?>
 			<table class="form-table">
@@ -268,7 +279,7 @@ function wcsh_settings_page(){
     		settings_fields( 'wcsh-settings-otherhooks' );
     		do_settings_sections( 'wcsh-settings-otherhooks' );
     		?>
-	  		<h2>WOOCOMMERCE OTHER HOOKS</h2>
+	  		<h2><?php _e('WOOCOMMERCE OTHER HOOKS', 'woo-simple-hooks'); ?></h2>
 	  		<?php
 	  		foreach ($otherhooks as $key => $otherhook){ ?>
 			<table class="form-table">
@@ -285,7 +296,7 @@ function wcsh_settings_page(){
     		settings_fields( 'wcsh-settings-shophook' );
     		do_settings_sections( 'wcsh-settings-shophook' );
     		?>
-    		<h2>WOOCOMMERCE SHOP PAGE HOOKS</h2>
+    		<h2><?php _e('WOOCOMMERCE SHOP PAGE HOOKS', 'woo-simple-hooks'); ?></h2>
     		<?php
 			foreach ($shophooks as $key => $shophook){	?>	  
 			<table class="form-table">
@@ -312,7 +323,7 @@ function wcsh_settings_page(){
 function wcsh_notice() {
     ?>
     <div class="error notice">
-    <p>Simple Hooks Plugin requires the WooCommerce Plugin</p>
+    <p><?php _e('Simple Hooks Plugin requires the WooCommerce Plugin', 'woo-simple-hooks'); ?></p>
     </div>
     <?php
 }
